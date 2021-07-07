d069aaa58bf0da1838100073273601a799e3bae1
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-07-07 10:42:23 +0700

    Use shell to avoid unexpected bugs

--- NetCat/netcat.py
+++ NetCat/netcat.py
@@ -12,7 +12,9 @@ def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return
-    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    output = subprocess.check_output(
+        shlex.split(cmd), stderr=subprocess.STDOUT, shell=True
+    )
     return output.decode()
 
 
