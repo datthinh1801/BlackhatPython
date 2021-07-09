982380ead1e2d00d85ce18a3540c6e500455ef36
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-07-09 09:20:21 +0700

    Complete an IP sniffer

--- Chapter 3/basic_sniffer.py
+++ Chapter 3/basic_sniffer.py
@@ -2,12 +2,10 @@ import socket
 import os
 import struct
 import ipaddress
-from ctypes import *
-
-HOST = socket.gethostbyname(socket.gethostname())
+import sys
 
 
-def main():
+def sniff(host: str):
     # Windows allows us to sniff all incoming packets regardless protocol
     if os.name == "nt":
         socket_protocol = socket.IPPROTO_IP
@@ -16,74 +14,39 @@ def main():
         socket_protocol = socket.IPPROTO_ICMP
 
     sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
-    sniffer.bind((HOST, 0))
+    sniffer.bind((host, 0))
     sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
     # if using Windows, turn on promiscuous mode
     if os.name == "nt":
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-    # read one packet
-    print(sniffer)
-    print(sniffer.recvfrom(65565))
+    # sniffing
+    try:
+        while True:
+            raw_packet = sniffer.recvfrom(65565)[0]
+            ip_header = IP(raw_packet)
+            # print the detected protocol and host
+            print(
+                f"Protocol: {ip_header.protocol}\t{ip_header.src_ip} ==> {ip_header.dst_ip}"
+            )
+            print({ip_header.src})
 
-    # if using Windows, turn off promiscuous mode
-    if os.name == "nt":
-        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
-
-
-class IP_ctypes(Structure):
-    """
-    A class represents an IP packet.
-
-    This class inherits the Structure class from the ctypes module.
-    When creating a new object, bits from the raw packet will be mapped to _fields_.
-    """
-
-    _fields_ = [
-        # 4 bit unsigned char
-        ("ihl", c_ubyte, 4),
-        # 4 bit unsigned char
-        ("version", c_ubyte, 4),
-        # 1 byte unsigned char
-        ("tos", c_ubyte, 8),
-        # 2 bytes unsigned short
-        ("len", c_ushort, 16),
-        # 2 bytes unsigned short
-        ("id", c_ushort, 16),
-        # 2 bytes unsigned short
-        ("offset", c_ushort, 16),
-        # 1 byte unsigned char
-        ("ttl", c_ubyte, 8),
-        # 1 byte unsigned char
-        ("protocol_num", c_ubyte, 8),
-        # 2 bytes unsigned short
-        ("sum", c_ushort, 16),
-        # 4 bytes unsigned int
-        ("src", c_uint32, 32),
-        # 4 bytes unsigned int
-        ("dst", c_uint32, 32),
-    ]
-
-    @classmethod
-    def __new__(cls, raw_packet=None):
-        """Parse the raw packet to pre-defined fields."""
-        return cls.from_buffer_copy(raw_packet)
-
-    def __init__(self, raw_packet=None):
-        # human readable IP addresses
-        self.src_ip = socket.inet_ntoa(struct.pack("<L", self.src))
-        self.dst_ip = socket.inet_ntoa(struct.pack("<L", self.dst))
+    except KeyboardInterrupt:
+        # if using Windows, turn off promiscuous mode
+        if os.name == "nt":
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        sys.exit(0)
 
 
-class IP_struct:
+class IP:
     """
     A class represents an IP packet using the struct module.
     """
 
-    def __init__(self, raw_packet=None):
+    def __init__(self, raw_packet):
         # Reference: https://docs.python.org/3/library/struct.html
-        header = struct.unpack("<BBHHHBBH4s4s", raw_packet)
+        header = struct.unpack("<BBHHHBBH4s4s", raw_packet[:20])
         self.ver = header[0] >> 4
         self.ihl = header[0] & 0xF
         self.tos = header[1]
@@ -91,7 +54,7 @@ class IP_struct:
         self.id = header[3]
         self.offset = header[4]
         self.ttl = header[5]
-        self.protocol = header[6]
+        self.protocol_num = header[6]
         self.sum = header[7]
         self.src = header[8]
         self.dst = header[9]
@@ -102,7 +65,26 @@ class IP_struct:
 
         # map protocol constants to their names
         self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except Exception as e:
+            print(f"No protocol for {(e, self.protocol_num)}")
+            self.protocol = str(self.protocol_num)
+
+
+class ICMP:
+    def __init__(self, raw_packet):
+        header = struct.unpack("<BBHHH", raw_packet[:8])
+        self.type = header[0]
+        self.code = header[1]
+        self.sum = header[2]
+        self.id = header[3]
+        self.seq = header[4]
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+    else:
+        host = "127.0.0.1"
+    sniff(host)
