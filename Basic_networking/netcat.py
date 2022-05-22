cff8a6b920bd4b4e5afd811380daac3d8036f162
Nguyễn Đạt Thịnh <44528004+datthinh1801@users.noreply.github.com>
2022-05-22 22:58:12 +0700

    Finalized netcat

--- Basic_networking/netcat.py
+++ Basic_networking/netcat.py
@@ -1,3 +1,6 @@
+from colorama import Fore, Style
+import getpass
+import os
 import argparse
 import socket
 import subprocess
@@ -34,19 +37,13 @@ def parse_arguments():
         type=int,
         required=True,
     )
-    parser.add_argument("--shell", help="open a command shell", action="store_true")
-    parser.add_argument(
-        "-e", "--execute", help="specify the command to execute", type=str
-    )
-    parser.add_argument("-t", "--target", help="specify the target IP", type=str)
-    parser.add_argument("-u", "--upload", help="upload a file", type=str)
     return parser.parse_args()
 
 
 class NetCat:
-    def __init__(self, args, buffer=None):
+    def __init__(self, args):
         self.args = args
-        self.buffer = buffer
+        self.buffer = ""
         self.buffer_size = 4096
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -58,11 +55,7 @@ class NetCat:
             self._send()
 
     def _send(self):
-        self.socket.connect((self.args.target, self.args.port))
-        # if the client has to send data to the server first
-        if self.buffer:
-            self.socket.send(self.buffer)
-
+        self.socket.connect((self.args.host, self.args.port))
         try:
             while True:
                 recv_len = 1
@@ -77,64 +70,106 @@ class NetCat:
                     if recv_len < self.buffer_size:
                         break
 
+                    if recv_len == 0:
+                        print(
+                            Fore.RED
+                            + "[*] Connection closed by the server"
+                            + Style.RESET_ALL
+                        )
+                        self.socket.close()
+                        exit()
+
                 if response:
-                    print(response)
-                    self.buffer = input("> ")
+                    print(response, end="")
+                    self.buffer = input().strip()
+                    if self.buffer == "exit":
+                        self.socket.close()
+                        sys.exit()
+
                     # the listener expects '\n' indicating the end of client's input
                     self.buffer += "\n"
                     self.socket.send(self.buffer.encode())
         except KeyboardInterrupt:
-            print("[*] Exiting...")
+            print(Fore.YELLOW + "[*] Exiting..." + Style.RESET_ALL)
             self.socket.close()
             sys.exit()
 
     def _listen(self):
         self.socket.bind((self.args.host, self.args.port))
         self.socket.listen(5)
+        print(
+            Fore.YELLOW
+            + f"[*] Listening on {self.args.host}:{self.args.port}"
+            + Style.RESET_ALL
+        )
 
         while True:
-            client_socket, _ = self.socket.accept()
-            client_thread = threading.Thread(target=self._handle, args=(client_socket,))
+            client_socket, client_addr = self.socket.accept()
+            print(
+                Fore.GREEN
+                + f"[*] Accepted connection from {client_addr[0]}:{client_addr[1]}"
+                + Style.RESET_ALL
+            )
+            client_thread = threading.Thread(
+                target=self._handle, args=(client_socket, client_addr)
+            )
             client_thread.start()
 
-    def _handle(self, client_socket: socket.socket):
+    def _handle(self, client_socket: socket.socket, client_addr: tuple):
         """
         Handle the client connection.
 
         Args:
             client_socket (socket): The client socket.
         """
-        if self.args.execute:
-            # execute a command specified by the client
-            output = execute(self.args.execute)
-            client_socket.send(output)
-        elif self.args.upload:
-            # receive a file from the client
-            file_buffer = b""
-            while True:
-                data = client_socket.recv(self.buffer_size)
-                if data:
-                    file_buffer += data
+        prompt = self._get_prompt()
+        client_socket.send(prompt.encode())
+
+        while True:
+            try:
+                cmd_buffer = ""
+                while "\n" not in cmd_buffer:
+                    buffer = client_socket.recv(self.buffer_size).decode()
+                    if len(buffer) == 0:
+                        print(
+                            Fore.RED
+                            + f"[*] Connection {client_addr[0]}:{client_addr[1]} closed by the client"
+                            + Style.RESET_ALL
+                        )
+                        client_socket.close()
+                        sys.exit()
+
+                    cmd_buffer += buffer
+
+                cmd_buffer = cmd_buffer.strip()
+                if cmd_buffer.startswith("cd "):
+                    try:
+                        os.chdir(cmd_buffer[3:])
+                        output = b""
+                    except FileNotFoundError:
+                        output = (
+                            Fore.RED + "[-] Directory not found.\n" + Style.RESET_ALL
+                        ).encode()
                 else:
-                    break
-            with open(self.args.upload, "wb") as f:
-                f.write(file_buffer)
-            message = f"Saved the file to {self.args.upload}"
-            client_socket.send(message.encode())
-        elif self.args.shell:
-            # open an interactive shell
-            while True:
-                try:
-                    client_socket.send(b"$ ")
-                    cmd_buffer = ""
-                    while "\n" not in cmd_buffer:
-                        cmd_buffer += client_socket.recv(self.buffer_size).decode()
-                    output = execute(cmd_buffer)
-                    client_socket.send(output)
-                except KeyboardInterrupt:
-                    print("[*] Exiting...")
-                    self.socket.close()
-                    sys.exit()
+                    try:
+                        output = execute(cmd_buffer)
+                    except FileNotFoundError:
+                        output = (
+                            Fore.RED + "[-] Command not found.\n" + Style.RESET_ALL
+                        ).encode()
+                output += self._get_prompt().encode()
+                client_socket.send(output)
+            except KeyboardInterrupt:
+                print("[*] Exiting...")
+                self.socket.close()
+                sys.exit()
+
+    @staticmethod
+    def _get_prompt():
+        username = getpass.getuser()
+        hostname = socket.gethostname()
+        prompt = f"{username}@{hostname}:{os.getcwd()}$ "
+        return Fore.GREEN + prompt + Style.RESET_ALL
 
 
 if __name__ == "__main__":
